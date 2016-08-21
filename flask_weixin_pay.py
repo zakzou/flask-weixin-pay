5ac7d37680e304f272da620fe793df4d3c83e01b
zwc <zwczou@gmail.com>
2016-08-21 13:36:32 +0800

    完善订单相关

--- flask_weixin_pay.py
+++ flask_weixin_pay.py
@@ -23,7 +23,7 @@ except ImportError:
     from xml.etree import ElementTree as etree
 
 
-__all__ = ("WeixinPay", "WeixinError")
+__all__ = ("WeixinPay", "WeixinPayError")
 __version__ = "0.1.0"
 __author__ = "Weicheng Zou <zwczou@gmail.com>"
 
@@ -31,10 +31,10 @@ __author__ = "Weicheng Zou <zwczou@gmail.com>"
 StandaloneApplication = namedtuple('StandaloneApplication', ['config'])
 
 
-class WeixinError(Exception):
+class WeixinPayError(Exception):
 
     def __init__(self, msg):
-        super(WeixinError, self).__init__(msg)
+        super(WeixinPayError, self).__init__(msg)
 
 
 class WeixinPay(object):
@@ -94,6 +94,12 @@ class WeixinPay(object):
     del _get_notify_url, _set_notify_url
 
     @property
+    def remote_addr(self):
+        if request is not None:
+            return request.remote_addr
+        return ""
+
+    @property
     def nonce_str(self):
         char = string.ascii_letters + string.digits
         return "".join(random.choice(char) for _ in range(32))
@@ -126,40 +132,51 @@ class WeixinPay(object):
             raw[child.tag] = child.text
         return raw
 
-    def unified_order(self, body, out_trade_no, total_fee, user_ip="",
-                      openid="", attach="", device_info="WEB", trade_type="JSAPI",
-                      *args, **kwargs):
-        url = "https://api.mch.weixin.qq.com/pay/unifiedorder"
-        if not user_ip and request is not None:
-            user_ip = request.remote_addr
-        params = {
-            "appid": self.app_id,
-            "attach": attach,
-            "device_info": device_info,
-            "body": body,
-            "mch_id": self.mch_id,
-            "nonce_str": self.nonce_str,
-            "notify_url": self.notify_url,
-            "out_trade_no": out_trade_no,
-            "spbill_create_ip": user_ip,
-            "total_fee": int(total_fee * 100),
-            "trade_type": trade_type,
-            "openid": openid,
-        }
-        params.update(kwargs)
-        params["sign"] = self.sign(params)
-        data = self.to_xml(params)
-        req = urllib2.Request(url, data=data)
+    def fetch(self, url, data):
+        req = urllib2.Request(url, data=self.to_xml(data))
         try:
             resp = self.opener.open(req, timeout=20)
         except urllib2.HTTPError, e:
             resp = e
-        row = self.to_dict(resp.read())
+        return self.to_dict(resp.read())
+
+    def unified_order(self, **data):
+        """
+        统一下单
+        out_trade_no、body、total_fee、trade_type必填
+        app_id, mchid, nonce_str自动填写
+        user_ip 在flask框架下可以自动填写
+        """
+        url = "https://api.mch.weixin.qq.com/pay/unifiedorder"
+
+        # 必填参数
+        if "out_trade_no" not in data:
+            raise WeixinPayError("缺少统一支付接口必填参数out_trade_no")
+        if "body" not in data:
+            raise WeixinPayError("缺少统一支付接口必填参数body")
+        if "total_fee" not in data:
+            raise WeixinPayError("缺少统一支付接口必填参数total_fee")
+        if "trade_type" not in data:
+            raise WeixinPayError("缺少统一支付接口必填参数trade_type")
+
+        # 关联参数
+        if data["trade_type"] == "JSAPI" and "openid" not in data:
+            raise WeixinPayError("trade_type为JSAPI时，openid为必填参数")
+        if data["trade_type"] == "NATIVE" and "product_id" not in data:
+            raise WeixinPayError("trade_type为NATIVE时，product_id为必填参数")
+        data.setdefault("appid", self.app_id)
+        data.setdefault("mch_id", self.mch_id)
+        data.setdefault("notify_url", self.notify_url)
+        data.setdefault("nonce_str", self.nonce_str)
+        data.setdefault("spbill_create_ip", self.remote_addr)
+        data.setdefault("sign", self.sign(data))
+
+        row = self.fetch(url, data)
         if row["return_code"] == "FAIL":
-            raise WeixinError(row["return_msg"])
+            raise WeixinPayError(row["return_msg"])
         err_msg = row.get("err_code_des")
         if err_msg:
-            raise WeixinError(err_msg)
+            raise WeixinPayError(err_msg)
         return row["prepay_id"]
 
     def jsapi(self, **kwargs):
@@ -172,3 +189,43 @@ class WeixinPay(object):
         sign = self.sign(raw)
         return dict(package=package, appId=self.app_id,
                     timeStamp=timestamp, nonceStr=nonce_str, sign=sign)
+
+    def order_query(self, **data):
+        """
+        订单查询
+        out_trade_no, transaction_id至少填一个
+        appid, mchid, nonce_str不需要填入
+        """
+        url = "https://api.mch.weixin.qq.com/pay/orderquery"
+
+        if "out_trade_no" not in data and "transaction_id" not in data:
+            raise WeixinPayError("订单查询接口中，out_trade_no、transaction_id至少填一个")
+        data.setdefault("appid", self.app_id)
+        data.setdefault("mch_id", self.mch_id)
+        data.setdefault("nonce_str", self.nonce_str)
+        data.setdefault("sign", self.sign(data))
+
+        row = self.fetch(url, data)
+        if row["return_code"] == "FAIL":
+            raise WeixinPayError(row["return_msg"])
+        return row
+
+    def close_order(self, out_trade_no):
+        """
+        关闭订单
+        transaction_id必填
+        appid, mchid, nonce_str不需要填入
+        """
+        url = "https://api.mch.weixin.qq.com/pay/closeorder"
+
+        data = {}
+        data.setdefault("out_trace_no", out_trade_no)
+        data.setdefault("appid", self.app_id)
+        data.setdefault("mch_id", self.mch_id)
+        data.setdefault("nonce_str", self.nonce_str)
+        data.setdefault("sign", self.sign(data))
+
+        row = self.fetch(url, data)
+        if row["return_code"] == "FAIL":
+            raise WeixinPayError(row["return_msg"])
+        return row
