589e9c20eee1d8336da4d0e169d1d5d81ccb1842
zwc <zaczwc@gmail.com>
2016-08-21 11:06:44 +0800

    fix bug

--- flask_weixin_pay.py
+++ flask_weixin_pay.py
@@ -63,7 +63,7 @@ class WeixinPay(object):
     def _set_app_id(self, app_id):
         self.app.config["WEIXIN_APP_ID"] = app_id
 
-    property("app_id", _set_app_id, _get_app_id)
+    app_id = property(_get_app_id, _set_app_id)
     del _set_app_id, _get_app_id
 
     def _get_mch_id(self):
@@ -72,7 +72,7 @@ class WeixinPay(object):
     def _set_mch_id(self, mch_id):
         self.app.config["WEIXIN_MCH_ID"] = mch_id
 
-    property("mch_id", _set_mch_id, _get_mch_id)
+    mch_id = property(_get_mch_id, _set_mch_id)
     del _get_mch_id, _set_mch_id
 
     def _get_mch_key(self):
@@ -81,7 +81,7 @@ class WeixinPay(object):
     def _set_mch_key(self, mch_key):
         self.app.config["WEIXIN_MCH_KEY"] = mch_key
 
-    property("mch_key", _get_mch_key, _set_mch_key)
+    mch_key = property(_get_mch_key, _set_mch_key)
     del _get_mch_key, _set_mch_key
 
     def _get_notify_url(self):
@@ -90,7 +90,7 @@ class WeixinPay(object):
     def _set_notify_url(self, notify_url):
         self.app.config["WEIXIN_NOTIFY_URL"] = notify_url
 
-    property("notify_url", _get_notify_url, _set_notify_url)
+    notify_url = property(_get_notify_url, _set_notify_url)
     del _get_notify_url, _set_notify_url
 
     @property
@@ -98,12 +98,14 @@ class WeixinPay(object):
         char = string.ascii_letters + string.digits
         return "".join(random.choice(char) for _ in range(32))
 
-    to_utf8 = lambda x: x.encode("utf-8") if isinstance(x, unicode) else x
+    to_utf8 = lambda self, x: x.encode("utf-8") if isinstance(x, unicode) else x
 
     def sign(self, raw):
-        s = "&".join("=".join(kv) for kv in raw.items())
-        s += "&key={1}".format(self.mch_key)
-        return hashlib.sha1(s.encode("utf-8")).hexdigest()
+        raw = [(k, raw[k]) for k in sorted(raw.keys())]
+        s = "&".join("=".join(kv) for kv in raw if kv[1])
+        s += "&key={0}".format(self.mch_key)
+        print s
+        return hashlib.md5(self.to_utf8(s)).hexdigest().upper()
 
     def verify(self, content):
         raw = self.to_dict(content)
@@ -114,7 +116,7 @@ class WeixinPay(object):
     def to_xml(self, raw):
         s = ""
         for k, v in raw.iteritems():
-            s += "<{0}><![CDATA[{1}]]</{0}>".format(k, self.to_utf8(v), k)
+            s += "<{0}>{1}</{0}>".format(k, self.to_utf8(v), k)
         return "<xml>{0}</xml>".format(s)
 
     def to_dict(self, content):
@@ -124,27 +126,30 @@ class WeixinPay(object):
             raw[child.tag] = child.text
         return raw
 
-    def unified_order(self, openid, body, out_trade_no, total_fee,
-                      user_ip=None, attach=None, *args, **kwargs):
+    def unified_order(self, body, out_trade_no, total_fee, user_ip=None,
+                      attach=None, device_info="WEB", openid="",
+                      trade_type="JSAPI", *args, **kwargs):
         url = "https://api.mch.weixin.qq.com/pay/unifiedorder"
         if user_ip is None and request is not None:
             user_ip = request.remote_addr
         params = {
             "appid": self.app_id,
             "attach": attach,
-            "device_info": "WEB",
+            "device_info": device_info,
             "body": body,
             "mch_id": self.mch_id,
             "nonce_str": self.nonce_str,
             "notify_url": self.notify_url,
             "out_trade_no": out_trade_no,
             "spbill_create_ip": user_ip,
-            "total_fee": str(total_fee * 100),
-            "trade_type": "JSAPI",
+            "total_fee": str(int(total_fee * 100)),
+            "trade_type": trade_type,
             "openid": openid,
         }
+        params.update(kwargs)
         params["sign"] = self.sign(params)
         data = self.to_xml(params)
+        print data
         req = urllib2.Request(url, data=data)
         try:
             resp = self.opener.open(req, timeout=20)
@@ -152,6 +157,7 @@ class WeixinPay(object):
             resp = e
         row = self.to_dict(resp.read())
         if row["return_code"] == "FAIL":
+            print row["return_msg"]
             raise WeixinError(row["return_msg"])
         return row["prepay_id"]
 
